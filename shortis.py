3a6811da0ab0e751deaf612d383d51a262765801
Kai Blin <kai@samba.org>
2013-04-24 14:36:37 +0200

    Run fine behind reverse proxy
    Use trick from http://flask.pocoo.org/snippets/35/ to support reverse proxies.
    
    Signed-off-by: Kai Blin <kai@samba.org>

--- shortis.py
+++ shortis.py
@@ -5,7 +5,38 @@ from redis import Redis
 import string
 import random
 
+class ReverseProxied(object):
+    '''
+    Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+
+        server = environ.get('HTTP_X_FORWARDED_SERVER', '')
+        if server:
+            environ['HTTP_HOST'] = server
+
+        return self.app(environ, start_response)
+
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 redis = Redis()
 
 def get_random_string(length=8):
